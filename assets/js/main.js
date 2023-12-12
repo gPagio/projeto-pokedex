78381e38fc8ed00b01455916ea50e965bb4802fa
gPagio <guiguipagio@gmail.com>
2023-12-12 05:54:34 -0300

    feat: inicio implementacao api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log('Front é coisa de retardado')
+const limit = 10
+const offset =0
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function (response){
+    console.log(response)
+})
