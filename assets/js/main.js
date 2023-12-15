633120c366784dd41a92ccb4ff16bfb1fe66c47b
gPagio <guiguipagio@gmail.com>
2023-12-14 23:17:20 -0300

    feat: implementacao tipo e cores por tipo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,24 @@
-const limit = 10
-const offset =0
+const limit = 20
+const offset = 0
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToList(pokemon){
     return `
-    <li class="pokemon">
-        <span class="number">#001</span>
+    <li class="pokemon ${pokemon.primaryType}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${pokemon.allTypes.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToList(pokemon)
-        }
-    })
-    .catch((error) => console.log(error))
+pokeApi.getPokemons(limit, offset).then((pokemons = []) => {
+    const listHtml = pokemons.map(convertPokemonToList).join(' ')
+    pokemonList.innerHTML += listHtml
+})
